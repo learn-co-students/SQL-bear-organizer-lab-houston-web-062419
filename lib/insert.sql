d04cae9797a39e7d8f3abbb155d8516efb8ddfe8
jordanbacon <jmbacon92@gmail.com>
2019-07-04 16:04:23 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 25, "M", "dark brown", "melancholic", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 5, "M", "light brown", "sanguine", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 20, "F", "medium brown", "choleric", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 15, "M", "medium brown", "phlegmatic", 1);
+INSERT INTO bears VALUES (5,"Melissa", 10, "F", "black", "melancholic", 1);
+INSERT INTO bears VALUES (6,"Grinch", 20, "M", "black", "choleric", 1);
+INSERT INTO bears VALUES (7,"Wendy", 15, "F", "dark brown", "phlegmatic", 1);
+INSERT INTO bears VALUES (8,null, 5, "F", "light brown", "sanquine", 1);
